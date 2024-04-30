e01a37f8cd206c4f9dcfc846d3693a87105e399f
theengineer115 <103286829+theengineer115@users.noreply.github.com>
2024-04-30 20:06:41 +0300

    Update nft-viewer.ts

--- nft-viewer.ts
+++ nft-viewer.ts
@@ -1,5 +1,7 @@
 import { ethers } from 'ethers';
-import * as infura from '@infura/sdk';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 interface NftOwnershipData {
   [tokenId: string]: string;
@@ -12,11 +14,12 @@ class NftViewer {
   private nftContractAddress: string;
   private erc721Abi: ethers.utils.Interface;
 
-  constructor(infuraProjectId: string, infuraApiKey: string, nftContractAddress: string) {
-    this.infuraProjectId = infuraProjectId;
-    this.infuraApiKey = infuraApiKey;
-    this.nftContractAddress = nftContractAddress;
-    this.provider = new ethers.providers.InfuraProvider(infuraProjectId, infuraApiKey);
+  constructor() {
+    // Accessing environment variables
+    this.infuraProjectId = process.env.INFURA_PROJECT_ID!;
+    this.infuraApiKey = process.env.INFURA_API_KEY!;
+    this.nftContractAddress = process.env.NFT_CONTRACT_ADDRESS!;
+    this.provider = new ethers.providers.InfuraProvider(this.infuraProjectId, this.infuraApiKey);
     this.erc721Abi = new ethers.utils.Interface([
       'function balanceOf(address owner) public view returns (uint256)',
       'function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256)',
